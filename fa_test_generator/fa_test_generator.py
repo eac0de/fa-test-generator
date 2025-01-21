d66d31a3c8924e6f9d852fa492255eea7255c327
egorzh <egor2001zhukov@gmail.com>
2025-01-21 18:29:57 +0300

    update 0.1.1

--- fa_test_generator/fa_test_generator.py
+++ fa_test_generator/fa_test_generator.py
@@ -9,12 +9,25 @@ from pydantic_core import PydanticUndefinedType
 
 
 class FATestGenerator:
-    def __init__(self, app: FastAPI, test_dir: str = "./tests/api/"):
+    def __init__(
+        self,
+        app: FastAPI,
+        test_dir: str | None = None,
+        base_url: str | None = None,
+    ):
+        if not isinstance(app, FastAPI):
+            raise ValueError("app must be an instance of FastAPI")
+        if not test_dir:
+            test_dir = "./tests/"
+        if test_dir[-1] != "/":
+            test_dir += "/"
         self.app = app
         self.test_dir = test_dir
+        self.base_url = base_url
 
     def generate(self):
         os.makedirs(self.test_dir, exist_ok=True)
+        os.makedirs(self.test_dir + "api/", exist_ok=True)
 
         routes_by_router: defaultdict[str, list[APIRoute]] = defaultdict(list)
         for route in self.app.routes:
@@ -23,8 +36,10 @@ class FATestGenerator:
                 routes_by_router[router_name].append(route)
 
         for router_name, routes in routes_by_router.items():
-            test_class_name = f"Test{''.join([word.capitalize() for word in router_name.split("_")])}"
-            test_file = os.path.join(self.test_dir, f"test_{router_name}.py")
+            test_class_name = (
+                f"Test{''.join([word.capitalize() for word in router_name.split("_")])}"
+            )
+            test_file = os.path.join(self.test_dir + "api/", f"test_{router_name}.py")
 
             test_methods = [self._generate_test_code(route) for route in routes]
             test_methods_str = "".join(test_methods)
@@ -37,6 +52,21 @@ class {test_class_name}:
                 test_class_code = "import jsony\n" + test_class_code
             with open(test_file, "w", encoding="utf-8") as f:
                 f.write(test_class_code)
+            conftest_code = f"""import pytest
+from asgi_lifespan import LifespanManager
+from httpx import AsyncClient, ASGITransport
+
+from main import app # change to your FastAPI app
+
+
+@pytest.fixture()
+async def client():
+    async with LifespanManager(app, startup_timeout=100, shutdown_timeout=100):
+        async with AsyncClient(transport=ASGITransport(app=app){", base_url=" + self.base_url if self.base_url else ""}) as ac:
+            yield ac"""
+            conftest_file = os.path.join(self.test_dir, "conftest.py")
+            with open(conftest_file, "w", encoding="utf-8") as f:
+                f.write(conftest_code)
 
     @staticmethod
     def _generate_test_code(route: APIRoute) -> str:
