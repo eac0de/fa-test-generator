6822a047a5b5bd1e46e34f4596b6117d843a885b
egorzh <egor2001zhukov@gmail.com>
2025-01-22 13:34:20 +0300

    update 0.1.2

--- fa_test_generator/fa_test_generator.py
+++ fa_test_generator/fa_test_generator.py
@@ -12,22 +12,20 @@ class FATestGenerator:
     def __init__(
         self,
         app: FastAPI,
-        test_dir: str | None = None,
-        base_url: str | None = None,
+        tests_dir: str | None = None,
     ):
         if not isinstance(app, FastAPI):
             raise ValueError("app must be an instance of FastAPI")
-        if not test_dir:
-            test_dir = "./tests/"
-        if test_dir[-1] != "/":
-            test_dir += "/"
+        if not tests_dir:
+            tests_dir = "./tests/"
+        if tests_dir[-1] != "/":
+            tests_dir += "/"
         self.app = app
-        self.test_dir = test_dir
-        self.base_url = base_url
+        self.tests_dir = tests_dir
 
     def generate(self):
-        os.makedirs(self.test_dir, exist_ok=True)
-        os.makedirs(self.test_dir + "api/", exist_ok=True)
+        os.makedirs(self.tests_dir, exist_ok=True)
+        os.makedirs(self.tests_dir + "api/", exist_ok=True)
 
         routes_by_router: defaultdict[str, list[APIRoute]] = defaultdict(list)
         for route in self.app.routes:
@@ -39,7 +37,7 @@ class FATestGenerator:
             test_class_name = (
                 f"Test{''.join([word.capitalize() for word in router_name.split("_")])}"
             )
-            test_file = os.path.join(self.test_dir + "api/", f"test_{router_name}.py")
+            test_file = os.path.join(self.tests_dir + "api/", f"test_{router_name}.py")
 
             test_methods = [self._generate_test_code(route) for route in routes]
             test_methods_str = "".join(test_methods)
@@ -52,36 +50,60 @@ class {test_class_name}:
                 test_class_code = "import jsony\n" + test_class_code
             with open(test_file, "w", encoding="utf-8") as f:
                 f.write(test_class_code)
-            conftest_code = f"""import pytest
+            conftest_code = f"""import os
+
+from dotenv import load_dotenv
+import pytest
 from asgi_lifespan import LifespanManager
 from httpx import AsyncClient, ASGITransport
 
 from main import app # change to your FastAPI app
 
+load_dotenv(dotenv_path="{self.tests_dir}test.env")
+
+@pytest.fixture(scope="session", autouse=True)
+def check_mode():
+    mode = os.getenv("MODE")
+    if mode != "TEST":
+        raise PermissionError(
+            f"Envrironment variable MODE must be TEST for testing. Current MODE = {"{"}mode{"}"}"
+        )
 
 @pytest.fixture()
 async def client():
     async with LifespanManager(app, startup_timeout=100, shutdown_timeout=100):
-        async with AsyncClient(transport=ASGITransport(app=app){", base_url=" + self.base_url if self.base_url else ""}) as ac:
+        async with AsyncClient(transport=ASGITransport(app=app), base_url="http://localhost") as ac:
             yield ac"""
-            conftest_file = os.path.join(self.test_dir, "conftest.py")
+            conftest_file = os.path.join(self.tests_dir, "conftest.py")
             with open(conftest_file, "w", encoding="utf-8") as f:
                 f.write(conftest_code)
+            pytest_ini_code = f"""[pytest]
+pythonpath = . .
+env_files = {self.tests_dir}test.env
+asyncio_mode=auto"""
+            pytest_ini_file = os.path.join(self.tests_dir, "pytest.ini")
+            with open(pytest_ini_file, "w", encoding="utf-8") as f:
+                f.write(pytest_ini_code)
+
+            env_test_code = "MODE=TEST"
+            pytest_ini_file = os.path.join(self.tests_dir, "test.env")
+            with open(pytest_ini_file, "w", encoding="utf-8") as f:
+                f.write(env_test_code)
 
     @staticmethod
     def _generate_test_code(route: APIRoute) -> str:
-        method = route.methods.pop()  # HTTP-метод
+        method = route.methods.pop()
         path = route.path
         test_method_name = f"test_{route.name}"
 
         if route.body_field:
             body_dct: dict[str, FieldInfo] = route.body_field.type_.model_fields
             body = {
-                k: f"title: {v.title}, annotation: {v.annotation} {f", default: {v.default}" if not isinstance(v.default, PydanticUndefinedType) else f", default_factory: {v.default_factory}" if v.default_factory else ""}"
+                k: f" annotation: {v.annotation} {f", title: {v.title}" if v.title else ''}{f", default: {v.default}" if not isinstance(v.default, PydanticUndefinedType) else f", default_factory: {v.default_factory}" if v.default_factory else ''}"
                 for k, v in body_dct.items()
             }
-            req = f"""data={"{"}
-{str(json.dumps(body, indent=12,ensure_ascii=False))[1: -1].strip("\n")}
+            req = f"""data = {"{"}
+{str(json.dumps(body, indent=12,ensure_ascii=False))[1: -1].strip("\n")},
         {"}"}
         resp = await client.{method.lower()}("{path}", json=jsony.normalize(data))"""
         else:
